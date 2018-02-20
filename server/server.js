eca64fcf4c65c11f4d9e18e2d3538a6e6e05e808
VinnyAsh <31900683+VinnyAsh@users.noreply.github.com>
2018-02-20 10:48:57 -0600

    Socket.io chat app init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
